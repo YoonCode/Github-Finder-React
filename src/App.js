1d84ed5eb72abea853c83f850ee2691fb68c8daf
YoonCode <yooncode>
2022-09-29 00:38:52 +0900

    change token

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Footer from './components/layout/Footer'
 import Alert from './components/layout/Alert'
 import Home from './pages/Home'
 import About from './pages/About'
+import User from './pages/User'
 import NotFound from './pages/NotFound'
 import { GithubProvider } from './context/github/GithubContext'
 import { AlertProvider } from './context/alert/AlertContext'
+
+// NOTE: Alert is only used on the '/' route moving to that route we can prevent
+// content shift when alert shows by hiding and unhiding the Alert rather than
+// conditionally rendering
 
 function App() {
   return (
@@ -16,13 +21,21 @@ function App() {
           <div className="flex flex-col justify-between h-screen">
             <Navbar />
 
-            <main className="mx-auto px-3 pb-12">
-              <Alert />
+            <main className="container mx-auto px-3 pb-12">
               <Routes>
-                <Route path="/" element={<Home />} />
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
-                <Route path="/*" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
               </Routes>
             </main>
 
